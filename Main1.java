50769c378cd3170d0071b9ef4dcad319683183ce
fedoseevatatyana <74595766+fedoseevatatyana@users.noreply.github.com>
2020-11-26 23:26:06 +0300

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int deposit = 1703;
+        int bonus = deposit / 100;
+        int initial = 100;
+        int overall = initial + deposit + bonus;
+        System.out.println(overall);
+    }
+}

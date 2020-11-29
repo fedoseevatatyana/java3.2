f8e4aa2810763e9678b357010cd5d4fb7702df6c
fedoseevatatyana <74595766+fedoseevatatyana@users.noreply.github.com>
2020-11-29 22:20:25 +0300

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,7 +1,12 @@
 public class Main1 {
     public static void main(String[] args) {
-        int deposit = 1703;
-        int bonus = deposit / 100;
+        int deposit = 1700;
+        int bonus;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
         int initial = 100;
         int overall = initial + deposit + bonus;
         System.out.println(overall);
